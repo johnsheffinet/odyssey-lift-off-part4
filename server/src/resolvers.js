2f2b60715ce58a9fd7bf0deb53d41b4eb03c6806
John William Sheffield Jr <john@sheffinet.org>
2022-12-12 21:12:46 -0500

    Update resolvers.js

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -26,15 +26,23 @@ const resolvers = {
   },
   Mutation: {
     // increments a track's numberOfViews property
-    incrementTrackViews: async (_, { id }, { dataSources }) => {
+    incrementTrackViews: async (_, {id}, {dataSources}) => {
+    try {
       const track = await dataSources.trackAPI.incrementTrackViews(id);
       return {
         code: 200,
         success: true,
         message: `Successfully incremented number of views for track ${id}`,
-        track,
+        track
       };
-    },
+    } catch (err) {
+      return {
+        code: err.extensions.response.status,
+        success: false,
+        message: err.extensions.response.body,
+        track: null
+      };
+    }
   },
 };
 
